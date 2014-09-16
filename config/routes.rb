fcf7234a02649ecb33e383c338746f72299138a0
David Kato <davidkbo@gmail.com>
2014-09-16 08:18:44 -0300

    Commit parcial para novo branch do simpleform

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
   
-  resources :users
   root  'static_pages#home'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   match '/signup', to: 'users#new', via:'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
