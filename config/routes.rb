6b029469c5363eb9a83a876584b2e96a04f3fc05
David Kato <davidkbo@gmail.com>
2014-09-18 09:16:03 -0300

    Finalizado Sign in e Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ SampleApp::Application.routes.draw do
   
   root  'static_pages#home'
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup', to: 'users#new', via:'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
